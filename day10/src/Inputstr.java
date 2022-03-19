e3c3be66d6f24cdb45b331ab201b74b04dabc4ce
Asus <Asus@DESKTOP-IFQP64L>
2022-03-19 11:46:05 -0700

    first commit

---
+++ day10/src/Inputstr.java
@@ -0,0 +1,12 @@
+//import java.io.InputStreamReader;
+//import java.io.BufferedReader;
+import java.io.*;
+public class Inputstr {
+	public static void main(String[] args) throws IOException {
+		InputStreamReader is=new InputStreamReader(System.in);
+		System.out.println("enter number");
+		BufferedReader br=new BufferedReader(is);
+		int num=Integer.parseInt(br.readLine());
+		System.out.println("you have entered "+num);
+		}
+}
