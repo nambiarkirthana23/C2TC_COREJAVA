18575a6d94cc2d9e3520c6d4137521a266341efe
Asus <Asus@DESKTOP-IFQP64L>
2022-03-20 00:01:54 -0700

    first commit

---
+++ day11/src/Junior_dev/CarModel.java
@@ -0,0 +1,10 @@
+package Junior_dev;
+
+public class CarModel {
+	public void display()
+	{
+		System.out.println("Tesla");
+		System.out.println("EMW");
+	}
+
+}
---
+++ day11/src/Senior_dev/Benz_Car.java
@@ -0,0 +1,9 @@
+package Senior_dev;
+import Junior_dev.CarModel;
+public class Benz_Car {
+public static void main(String[] args)
+{
+	CarModel c=new CarModel();
+	c.display();
+}
+}
