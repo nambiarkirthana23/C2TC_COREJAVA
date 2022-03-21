59b207698f5b92d6920d8511e8368e6b4c9e4759
Asus <Asus@DESKTOP-IFQP64L>
2022-03-21 16:37:43 -0700

    first commit

---
+++ keywords/src/LongKeyword.java
@@ -0,0 +1,10 @@
+
+public class LongKeyword {
+	public static void main(String[] args)  
+	{  
+	long num1=20L;  
+	long num2=-20L;  
+	System.out.println("num1: "+num1);  
+	System.out.println("num2: "+num2);  
+	}  
+}
