e088c90d01f90cda98fc6a30bf7091f938c57481
Asus <Asus@DESKTOP-IFQP64L>
2022-03-21 16:57:26 -0700

    first commit

---
+++ keywords/src/Newkeyword.java
@@ -0,0 +1,11 @@
+public class Newkeyword {
+	void display()  
+    {  
+        System.out.println("Invoking Method");  
+    }  
+      
+    public static void main(String[] args) {  
+        NewKeyword nk=new NewKeyword();
+        nk.display();
+    }  
+}
