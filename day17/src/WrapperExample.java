dbc012f85afdd00ad62b535fc9d56ce57994d469
Asus <Asus@DESKTOP-IFQP64L>
2022-03-21 15:10:35 -0700

    first commit

---
+++ day17/src/WrapperExample.java
@@ -0,0 +1,10 @@
+
+public class WrapperExample {
+public static void main(String[] args)
+{
+	int a=20;
+	Integer i=a; //converting  int to Integer
+	Integer j=new Integer(30);
+	System.out.println(a+" "+i+" "+j);  
+}
+}
