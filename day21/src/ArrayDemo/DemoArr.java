8111cfef946613d86172ed7b8485d70fe4afb1cb
Asus <Asus@DESKTOP-IFQP64L>
2022-03-22 14:26:24 -0700

    first commit

---
+++ day21/src/ArrayDemo/DemoArr.java
@@ -0,0 +1,18 @@
+package ArrayDemo;
+
+public class DemoArr 
+{
+	public static void main(String args[])
+	{
+		int a[]=new int[5];    //intialize array
+	a[0]=34;
+	a[1]=12;
+	a[2]=3;
+	a[3]=78;
+    a[4]=67;
+    for(int i=0;i<a.length;i++)
+    {
+    	System.out.println(a[i]);
+    }
+}
+}
