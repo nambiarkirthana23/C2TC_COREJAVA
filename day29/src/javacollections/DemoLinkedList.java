1ea0529974c4ef770afa8a3431b453d69bea552d
Asus <Asus@DESKTOP-IFQP64L>
2022-04-03 16:03:25 -0700

    first commit

---
+++ day29/src/javacollections/DemoLinkedList.java
@@ -0,0 +1,19 @@
+package javacollections;
+import java.util.LinkedList;   
+
+public class DemoLinkedList {
+	public static void main(String[] args) {
+		LinkedList<Integer>list1=new LinkedList();
+		list1.add(10);
+		list1.add(20);
+		list1.add(30);
+		list1.add(40);                 //add element
+		list1.add(50);
+		list1.add(60);
+		for(int element:list1) 
+		System.out.println(element);
+		System.out.println("Peek the First element in the list::"+list1.peek());   
+		System.out.println("Remove the third element::"+list1.remove(3));          //delete element
+		
+}
+}
