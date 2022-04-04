445a1841034ae7861d3968dd79502015b0bb5e0a
Asus <Asus@DESKTOP-IFQP64L>
2022-04-03 18:16:37 -0700

    first commit

--- day29/src/javacollections2/ArrayListDemo.java
+++ day29/src/javacollections2/ArrayListDemo.java
@@ -5,11 +5,11 @@ public class ArrayListDemo {
 		ArrayList<String>list1=new ArrayList();
 		
 		list1.add("java");              //insert the value in a list
-		list1.add("	C");
-		list1.add("	python");
-		list1.add("	C++");
-		list1.add("	PHP");
-		list1.add("	.Net");
+		list1.add("C");
+		list1.add("python");
+		list1.add("C++");
+		list1.add("PHP");
+		list1.add(".Net");
 		System.out.println("Following are List of Programing Langauges::"+"\n"+list1);
 		
 		list1.remove(0);          // delete the value in a list
