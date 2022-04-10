0ba7cdbc6fd9c158a266f6d526c83dcf9465ac6c
Asus <Asus@DESKTOP-IFQP64L>
2022-04-10 16:58:10 -0700

    first commit

---
+++ WeekendAssignment/src/RightAngleTriangle.java
@@ -0,0 +1,19 @@
+
+public class RightAngleTriangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][][] arr = {
+	             {
+	          	  {1},{2,2},{3,3,3},{4,4,4,4}}
+	            };
+
+         for(int i=1;i<=4;i++)
+         {
+         for(int j=1;j<=i;j++)
+          System.out.print(i);
+        System.out.println("");
+        }
+      }
+	}
+
