efcf3d66c9cd4e8ad90a88bc0187bd5d7a42d746
Asus <Asus@DESKTOP-IFQP64L>
2022-04-10 16:56:38 -0700

    first commit

---
+++ WeekendAssignment/src/SwapTwoNumber.java
@@ -0,0 +1,24 @@
+
+public class SwapTwoNumber {
+
+	public static void main(String[] args) {
+		//swapping two number without third variable
+		/* int a=10,b=20;
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.print(a+" "+b);
+		*/
+		//swapping two number using third variable
+		int a=10,b=20;
+		int c;
+		c=a;
+		a=b;
+		b=c;
+		System.out.print(a+" "+b);
+		
+		
+
+	}
+
+}
