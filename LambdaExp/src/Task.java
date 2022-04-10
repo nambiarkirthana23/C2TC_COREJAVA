20981fe55c3ab2985fabc626f262caf5392185bd
Asus <Asus@DESKTOP-IFQP64L>
2022-04-10 01:07:45 -0700

    first commit

--- LambdaExp/src/Task.java
+++ LambdaExp/src/Task.java
@@ -1,13 +1,11 @@
- interface Pi
+interface Pi
 {
 	public double getPivalue();
 }
 public class Task {
-
 	public static void main(String[] args) {
-	   Pi i;
-       i=()->3.14;
-       System.out.println("value of pi ="+i.getPivalue());
-	}
-
+		   Pi i;
+	       i=()->3.14;
+	       System.out.println("value of pi ="+i.getPivalue());
+		}
 }
