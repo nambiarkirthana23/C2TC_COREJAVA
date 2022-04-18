40da5a79e1a51a4c2d3d16e69abbebd0b8adb643
Asus <Asus@DESKTOP-IFQP64L>
2022-04-18 04:38:12 -0700

    first commit

---
+++ Sychronize/src/DemoDateofTime.java
@@ -0,0 +1,30 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.ZoneId;
+
+public class DemoDateofTime {
+
+	public static void main(String[] args)  {
+	
+       LocalDate ld=LocalDate.now();
+       System.out.println(ld);
+     
+		LocalTime ld1=LocalTime.now();
+       System.out.println(ld1);
+     
+		LocalDateTime ld2=LocalDateTime.now();
+		System.out.println(ld2);
+		
+		LocalDateTime ld3=LocalDateTime.of(1008,05,20,0,0);
+		System.out.println(ld3);
+		
+		LocalDateTime ld4=LocalDateTime.of(1008,Month.APRIL,20,0,0);
+		System.out.println(ld4);
+		
+		LocalDateTime ld5=LocalDateTime.now(ZoneId.of("GMI"));
+		System.out.println(ld5);
+	}
+
+}
