2bc47b334f81877ec0a14a7837337013b0fb40bf
Asus <Asus@DESKTOP-IFQP64L>
2022-05-02 02:21:46 -0700

    first commit

---
+++ BankApp/src/com/tnsif/Framework/ShopAcc.java
@@ -0,0 +1,40 @@
+package com.tnsif.Framework;
+
+public class ShopAcc {
+	protected int accNo;
+	protected String accNm;
+	protected float charges;
+	
+	public int getAccNo() {
+		return accNo;
+	}
+	public void setAccNo(int accNo) {
+		this.accNo = accNo;
+	}
+	public String getAccNm() {
+		return accNm;
+	}
+	public void setAccNm(String accNm) {
+		this.accNm = accNm;
+	}
+	public float getCharges() {
+		return charges;
+	}
+	public void setCharges(float charges) {
+		this.charges = charges;
+	}
+	public void bookProduct(float charges) {
+
+	}
+		public void items(float charges) {
+			
+		}
+		
+		@Override
+		public String toString() {
+			return "ShopAcc [accNo=" + accNo + ", accNm=" + accNm + ", charges=" + charges + "]";
+		}
+		
+	
+	
+	}
---
+++ BankApp/src/com/tnsif/Framework/ShopFactory.java
@@ -0,0 +1,6 @@
+package com.tnsif.Framework;
+
+public interface ShopFactory {
+	public abstract PrimeAcc getNewPrimeAccount(int accNo,String accNm,float charges,float isPrime);
+	public abstract NormalAcc getNewNormalAccount(int accNo,String accNm,float charges,float deliveryCharge);
+}
